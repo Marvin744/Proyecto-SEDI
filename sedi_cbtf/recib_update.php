38a6fc2396339586885a393584ca2c4277d5fb1b
Marvin Meza <106187384+Marvin744@users.noreply.github.com>
2024-06-28 20:57:14 -0600

    Add files via upload

---
+++ sedi_cbtf/recib_update.php
@@ -0,0 +1,18 @@
+<?php
+include_once '../bd/conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar(); 
+
+
+$id_alerta = (isset($_POST['id_alerta'])) ? $_POST['id_alerta'] : '';
+
+$tipo_alerta = (isset($_POST['tipo_alerta'])) ? $_POST['tipo_alerta'] : '';
+
+
+$update = ("UPDATE alertas SET tipo_alerta = '$tipo_alerta' WHERE id_alerta = $id_alerta;");
+$query = $conexion->prepare($update);
+$query->execute();
+$mostrar = $query->fetch();
+
+header('Location: alerta.php'); die;
+?>
