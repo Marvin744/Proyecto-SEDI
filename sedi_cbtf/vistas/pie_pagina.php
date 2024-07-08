475352f66d6b6e548850bbfdb1781a3150c7d7e6
HendrikVillarreal <106553943+HendrikVillarreal@users.noreply.github.com>
2024-07-08 00:23:23 -0600

    Delete sedi_cbtf directory

--- sedi_cbtf/vistas/pie_pagina.php
+++
@@ -1,7 +0,0 @@
-</main>
-<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <script src="/script.js"></script>
-<h6>Este es el pie de página</h6>
-  </body>
-</html>
